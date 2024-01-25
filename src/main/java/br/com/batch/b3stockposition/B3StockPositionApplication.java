ddbc5d5c24cf37b83ff853b3ba335632a025107b
Henrique Kisaki <satoshikisaki@hotmail.com>
2024-01-25 20:07:37 -0300

    First commit

---
+++ src/main/java/br/com/batch/b3stockposition/B3StockPositionApplication.java
@@ -0,0 +1,13 @@
+package br.com.batch.b3stockposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B3StockPositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B3StockPositionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/batch/b3stockposition/B3StockPositionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batch.b3stockposition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3StockPositionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
